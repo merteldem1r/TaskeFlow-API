ba084b30356a0db247c61909de24c0df38db956d
Mert Eldemir <mert1greatest@gmail.com>
2025-12-09 19:21:20 +0300

    last updates

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"database/sql/driver"
 	"fmt"
+	"log"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 )
 
 func main() {
-	fmt.Println("Golang backend")
+	conn, err := connect()
+
+	if err != nil {
+		log.Fatalf("failed to connect to ClickHouse: %v", err)
+	}
+
+
+}
+
+func connect() (driver.Conn, error) {
+	var (
+		ctx = context.Background()
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{"<CLICKHOUSE_SECURE_NATIVE_HOSTNAME>:9440"},
+			Auth: clickhouse.Auth{
+				
+			}
+		})
+		
+	) 
 }
